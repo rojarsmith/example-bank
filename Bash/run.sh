450577ed0334e11cc8efb0b1048868d789828ea0
Rojar Smith <rojarsmith@gmail.com>
2022-01-31 23:12:57 +0800

    Add for in bash

--- Bash/run.sh
+++ Bash/run.sh
@@ -4,3 +4,23 @@
 echo `dirname $0`
 
 echo `pwd`
+
+## for
+
+for i in `seq 5 10`;
+do
+    echo $i
+done
+
+str="this example 
+demonstrates the for loop"
+
+for line in "$str"
+do
+   echo "$line"
+done
+
+for i in $str
+do
+   echo "$i"
+done
